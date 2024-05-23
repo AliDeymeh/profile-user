aa7aadc98920143125f92a10d7473a474c60a538
Ali_Deymeh <alideymeh9@gmail.com>
2024-05-23 12:36:56 +0330

    fix api send data

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+document
+  .getElementById("feedbackForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    console.log("object");
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const subject = document.getElementById("subject").value;
+    const message = document.getElementById("message").value;
+
+    fetch("http://localhost/portiflo/api-save-data.php", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        subject: subject,
+        message: message,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        alert(data.message);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        alert(error);
+      });
+  });
